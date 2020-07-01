4064fcd366c699f4eaefeb7b7320694ffc4f9357
BrainyXS <brainyxs@gmail.com>
2020-07-01 22:14:00 +0200

    CreatedProject

---
+++ Assets/MyGameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGameManager : MonoBehaviour
+{
+    public GameObject[] GameObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(GameObjects[0]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/followPlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 ofset;
+
+    private void Update()
+    {
+        transform.position = new Vector3(player.position.x + ofset.x, player.position.y + ofset.y, player.position.z + ofset.z);
+    }
+}
